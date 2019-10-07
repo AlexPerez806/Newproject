92ab3ad7cb5ecf22f85e7939bc2473ebed5a4ba5
Alexperez806 <sgtflyalf123@gmail.com>
2019-10-07 15:28:59 -0500

    part 2 of today

--- newProject.py
+++ newProject.py
@@ -7,3 +7,17 @@ y = "Those who know %s and those who %s" % (binary, doNot)
 
 print(x)
 print(y)
+
+print("I said: %r.:" % x)
+print("I also said: '%s'.")
+
+hilarius = True
+jokeEvolution = "Isn't that joke so funny?! %r"
+
+print(jokeEvolution % hilarius)
+
+w = "This is the left side of..."
+e = "a string with a right side."
+print(w+e)
+
+# More printing fun
