47093abbec25381d9ccb8222ce82f7eceb9c8a70
Alexperez806 <sgtflyalf123@gmail.com>
2019-10-09 15:33:38 -0500

    1st commit of 10/9

--- newProject.py
+++ newProject.py
@@ -41,3 +41,78 @@ end12 = "r"
 print(end1 + end2 + end3 + end4 + end5 + end6)
 print(end7 + end8 + end9 + end10 + end11 + end12)
 
+# But wait! There's more:
+formatter = "%r %r %r"
+print(formatter % (1, 2, 3, 4))
+print(formatter % ("one", "two", "three", "four"))
+print(formatter % (True, False, False, True))
+print(formatter % (formatter, formatter, formatter, formatter))
+
+# why do I use %r instead of %s in the above example
+
+# which should I use on a regular basis?
+
+# why does %r sometimes give me single quotes around things?
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the days: ", months)
+
+print("""
+There's something going on here.
+With the three doubles-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
+
+# examine closely the differance
+print("Here are the months: %r")
+
+
+# escape sequences redux
+
+
+
+
+
+
+
+
+
+
+
+
+print(backslashCat)
+print(topCat)
+
+# Escape Seq       What it does?
+# \\
+# \'
+# \"
+# \a
+# \b
+# \f
+# \n
+# \N{name}
+# \r
+# \t
+# \uxxxx
+# \Uxxxxxxxx
+# \v
+# \ooo
+# \xhh
+
+# What does the following code block do:
+# while True:
+#      for i in ["/", "-", "|", "\\", "|"]:
+#           print("%s\r" % i, end='')
+
+# Can you use ''' instead of """ ?
+
+age = input("How old are you?")
+height = input("How tall are you?")
+
+print("So, you are %r old and %r tall." %(age, height))
+
